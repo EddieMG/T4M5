6e902a085128f375c3aa5fb0144f867848a054cc
EddieMG <eduardmgobierno@gmail.com>
2020-02-29 16:49:47 +0100

    first impl
    Signed-off-by: EddieMG <eduardmgobierno@gmail.com>

--- w1_code_hermes.py
+++ w1_code_hermes.py
@@ -1,3 +1,134 @@
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
 import torch
 import torchvision
+import torchvision.transforms as transforms
+import torch.utils.data
+import matplotlib.pyplot as plt
+import numpy as np
 
+trainset_path ='/home/mcv/datasets/MIT_split/train/'
+testset_path ='/home/mcv/datasets/MIT_split/test/'
+
+batch_size = 4
+
+trans = transforms.Compose([
+        transforms.Resize((32, 32)),
+        transforms.ToTensor()
+     ])
+
+trainset = torchvision.datasets.ImageFolder(root=trainset_path , transform = trans)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                      shuffle=True, num_workers=2)
+
+
+testset = torchvision.datasets.ImageFolder(root=testset_path, transform = trans)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ['Opencountry', 'coast', 'forest', 'highway', 'inside_city', 'mountain', 'street', 'tallbuilding']
+
+
+# functions to show an image
+
+def imshow(img):
+    #img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# # get some random training images
+# dataiter = iter(trainloader)
+# images, labels = dataiter.next()
+# print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+# # show images
+# imshow(torchvision.utils.make_grid(images))
+# # print labels
+
+
+
+class PolNet(nn.Module):
+    def __init__(self):
+        super(PolNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = PolNet()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+EPOCHS = 2
+
+for epoch in range(EPOCHS):  # loop over the dataset multiple times
+    print("Epoch "+str(epoch))
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the test images: %d %%' % (
+    100 * correct / total))
+
+class_correct = list(0. for i in range(len(classes)))
+class_total = list(0. for i in range(len(classes)))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        print("labels: ", labels)
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(batch_size):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+for i in range(len(classes)):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
