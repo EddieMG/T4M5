1693fc176104d4ade10cc78285921578ff215149
polalbacar <pol.albacar@hotmail.com>
2020-03-15 21:50:04 +0100

    Merged stashed changes

--- w3/datasets.py
+++ w3/datasets.py
@@ -2,12 +2,14 @@ from pathlib import Path
 from itertools import chain
 import numpy as np
 from pycocotools.mask import toBbox
+from pycocotools import coco
+from pycocotools import cocoeval
 from detectron2.structures import BoxMode
 from mots_tools_io import (
     load_txt,
 )  # download and rename from https://github.com/VisualComputingInstitute/mots_tools/blob/master/mots_common/io.py
 
-base_dir = Path("/data")
+base_dir = Path("/home/mcv/datasets")
 
 
 def yield_dicts(img_dir, lbl_path):
@@ -17,13 +19,13 @@ def yield_dicts(img_dir, lbl_path):
         return bbox
 
     objs = load_txt(lbl_path)
-
     for img_path in img_dir.iterdir():
         if img_path.suffix not in [".jpg", ".png"]:
             continue
         idx = int(img_path.stem)
         if idx not in objs or len(objs[idx]) == 0:
             continue
+
         record = {
             "file_name": str(img_path),
             "image_id": idx,
@@ -31,7 +33,7 @@ def yield_dicts(img_dir, lbl_path):
             "width": objs[idx][0].mask["size"][1],
             "annotations": [
                 {
-                    "bbox": to_XYXY(toBbox(obj.mask)),
+                    "bbox": to_XYXY(toBbox(obj.mask)).tolist(),
                     "bbox_mode": BoxMode.XYXY_ABS,
                     "category_id": obj.class_id - 1,
                     "iscrowd": 0,
@@ -40,6 +42,7 @@ def yield_dicts(img_dir, lbl_path):
                 if obj.class_id != 10
             ],
         }
+
         yield record
 
 
@@ -88,17 +91,51 @@ def register_datasets():
 if __name__ == "__main__":
     import random
     import cv2
+    import os
     from detectron2.data import MetadataCatalog, DatasetCatalog
     from detectron2.utils.visualizer import Visualizer
+    from detectron2.evaluation import COCOEvaluator, inference_on_dataset, LVISEvaluator
+    from detectron2.data import build_detection_test_loader
+    from detectron2.config import get_cfg
+    from detectron2 import model_zoo
+    from detectron2.engine import DefaultTrainer
 
     thing_classes = ["Car", "Pedestrian"]
-    DatasetCatalog.register("mots", get_MOTS_dicts)
-    DatasetCatalog.register("kitti-mots", get_MOTS_dicts)
-    MetadataCatalog.get("mots").set(thing_classes=thing_classes)
+
+    #DatasetCatalog.register("mots", get_MOTS_dicts)
+    #MetadataCatalog.get("mots").set(thing_classes=thing_classes)
+
+    DatasetCatalog.register("kitti-mots", get_KITTI_MOTS_dicts)
     MetadataCatalog.get("kitti-mots").set(thing_classes=thing_classes)
 
-    dataset_dicts = get_KITTI_MOTS_dicts()
-    for d in random.sample(dataset_dicts, 5):
+    #dataset_dicts = get_MOTS_dicts()
+
+    # We train the model with the weight initialized.
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/retinanet_R_101_FPN_3x.yaml"))
+    cfg.DATASETS.TRAIN = ("kitti-mots",)
+    cfg.DATASETS.TEST = ()
+    cfg.DATALOADER.NUM_WORKERS = 2
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/retinanet_R_101_FPN_3x.yaml")  # Let training initialize from model zoo
+    cfg.SOLVER.IMS_PER_BATCH = 2
+    cfg.SOLVER.BASE_LR = 0.0025  # pick a good LR
+    cfg.SOLVER.MAX_ITER = 6000  # 300 iterations seems good enough for this toy dataset; you may need to train longer for a practical dataset
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512  # faster, and good enough for this toy dataset (default: 512)
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2
+
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+    trainer = DefaultTrainer(cfg)
+    trainer.resume_or_load(resume=False)
+    trainer.train()
+
+    # EVALUATION
+    evaluator = COCOEvaluator("kitti-mots", cfg, False, output_dir="./output/")
+    val_loader = build_detection_test_loader(cfg, "kitti-mots")
+    inference_on_dataset(trainer.model, val_loader, evaluator)
+    # another equivalent way is to use trainer.test
+
+
+    '''for d in random.sample(dataset_dicts, 5):
         img = cv2.imread(d["file_name"])
         visualizer = Visualizer(
             img[:, :, ::-1], metadata=MetadataCatalog.get("mots"), scale=0.5
@@ -106,4 +143,4 @@ if __name__ == "__main__":
         vis = visualizer.draw_dataset_dict(d)
         cv2.imshow("here", vis.get_image()[:, :, ::-1])
         cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        cv2.destroyAllWindows()'''
